36d8e79e492bd712a39e27509a3d71a1dcf46f9e
Dattito <d.siregar@web.de>
2021-10-04 23:21:32 +0200

    implemented better logging

--- src/api/providers/rest/controllers/account_info.go
+++ src/api/providers/rest/controllers/account_info.go
@@ -8,6 +8,7 @@ import (
 	"github.com/dattito/purrmannplus-backend/app/commands"
 	"github.com/dattito/purrmannplus-backend/app/models"
 	"github.com/dattito/purrmannplus-backend/config"
+	"github.com/dattito/purrmannplus-backend/logging"
 	"github.com/dattito/purrmannplus-backend/services/signal_message_sender"
 	utils_jwt "github.com/dattito/purrmannplus-backend/utils/jwt"
 	"github.com/gofiber/fiber/v2"
@@ -17,7 +18,7 @@ import (
 func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 	pr := new(api_models.PostSendPhoneNumberConfirmationLinkRequest)
 	if err := c.BodyParser(pr); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"error": err.Error(),
 		})
 	}
@@ -28,7 +29,7 @@ func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 
 	ok, err := commands.ValidAccountId(accountId)
 	if err != nil {
-		log.Println(err.Error())
+		logging.Errorf("%v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
 			"error": "Something went wrong",
 		})
@@ -42,13 +43,14 @@ func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 
 	account_info, err := models.NewAccountInfo(models.Account{Id: accountId}, pr.PhoneNumber)
 	if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"error": err.Error(),
 		})
 	}
 
 	has_phone_number, err := commands.HasPhoneNumber(accountId)
 	if err != nil {
+		logging.Errorf("%v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
 			"error": "Something went wrong",
 		})
@@ -62,6 +64,7 @@ func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 
 	token, err := utils_jwt.NewAccountIdPhoneNumberToken(account_info.Account.Id, account_info.PhoneNumber)
 	if err != nil {
+		logging.Errorf("%v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
 			"error": "couln't create token",
 		})
@@ -75,7 +78,7 @@ func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 	err = signal_message_sender.SignalMessageSender.Send(text, account_info.PhoneNumber)
 
 	if err != nil {
-		log.Println(err.Error())
+		logging.Errorf("%v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
 			"error": "couln't send signal message",
 		})
@@ -87,21 +90,22 @@ func SendPhoneNumberConfirmationLink(c *fiber.Ctx) error {
 func AddPhoneNumber(c *fiber.Ctx) error {
 	p := new(api_models.PostAddPhoneNumberRequest)
 	if err := c.QueryParser(p); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
 			"error": err.Error(),
 		})
 	}
 
 	if p.Token == "" {
 		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
-			"error": "token is required",
+			"error": "Token is required",
 		})
 	}
 
 	accountId, phoneNumber, err := utils_jwt.ParseAccountIdPhoneNumberToken(p.Token)
 	if err != nil {
+		logging.Errorf("%v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
-			"error": "couln't parse token",
+			"error": "Couln't parse token",
 		})
 	}
 
@@ -119,10 +123,17 @@ func AddPhoneNumber(c *fiber.Ctx) error {
 		})
 	}
 
-	if _, err = commands.AddAccountInfo(accountId, phoneNumber); err != nil {
-		log.Println(err.Error())
+	_, user_err, internal_error := commands.AddAccountInfo(accountId, phoneNumber)
+	if internal_error != nil {
+		logging.Errorf("%v", internal_error)
 		return c.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
 			"error": "Something went wrong",
+		})
+	}
+
+	if user_err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"error": user_err.Error(),
 		})
 	}
 
